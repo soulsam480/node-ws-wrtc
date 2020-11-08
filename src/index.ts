65852476df202155b2b601268637dc978ba2bfb5
sambit sahoo <soulsam480@hotmail.com>
2020-11-08 20:18:03 +0530

    two way rtc data success

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Server } from "socket.io";
 
 const io = new Server();
-const activeSockets: string[] = []
-io.listen(4000);
+let activeSockets: string[] = []
+
+io.listen(3000);
 
 io.on("connection", (socket) => {
     const existingSocket = activeSockets.find(
@@ -29,6 +30,24 @@ io.on("connection", (socket) => {
         });
     });
 
+    socket.on("make-answer", (data: any) => {
+        socket.to(data.to).emit("answer-made", {
+            socket: socket.id,
+            answer: data.answer
+        });
+    });
+    socket.on("text-data", (data: any) => {
+        console.log(data.text)
+    })
+
+    socket.on("disconnect", () => {
+        activeSockets = activeSockets.filter(
+            existingSocket => existingSocket !== socket.id
+        );
+        socket.broadcast.emit("remove-user", {
+            socketId: socket.id
+        });
+    });
 
 })
 
