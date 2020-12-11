47ac08118394baa560ec46f431dda1b08b05bf09
sambit sahoo <soulsam480@hotmail.com>
2020-12-12 01:50:18 +0530

    feat: adding proper client

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,75 @@
-import { Server } from "socket.io";
-import express from "express";
+import { Server, Socket } from 'socket.io';
+import express from 'express';
 
 const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-const server = app.use("/", (req, res) => res.send("<p>connected</p>")).listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = app
+  .use('/', (req, res) => res.send('<p>connected</p>'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = new Server(server);
 
+interface User {
+  name: string;
+  id: string;
+}
 
-let activeSockets: string[] = []
+let activeSockets: User[] = [];
 
+io.on('connection', (socket: Socket) => {
+  let existingSocket: User;
+  socket.on('add-user', (data: any) => {
+    existingSocket = {
+      name: data.name,
+      id: socket.id,
+    };
 
-io.on("connection", (socket) => {
-    const existingSocket = activeSockets.find(
-        existingSocket => existingSocket === socket.id
-    );
-
-    if (!existingSocket) {
-        activeSockets.push(socket.id)
-
-        socket.emit("update-user-list", {
-            users: activeSockets.filter(
-                existingSocket => existingSocket !== socket.id
-            )
-        })
-
-        socket.broadcast.emit("update-user-list", {
-            users: [socket.id]
-        })
+    if (!activeSockets.find((el) => el.id === existingSocket.id)) {
+      activeSockets.push(existingSocket);
     }
-    socket.on("call-user", (data: any) => {
-        socket.to(data.to).emit("call-made", {
-            offer: data.offer,
-            socket: socket.id
-        });
+
+    socket.emit('update-user-list', {
+      users: activeSockets.filter(
+        (existingSocket) => existingSocket.id !== socket.id,
+      ),
     });
 
-    socket.on("make-answer", (data: any) => {
-        socket.to(data.to).emit("answer-made", {
-            socket: socket.id,
-            answer: data.answer
-        });
+    socket.broadcast.emit('update-user-list', {
+      users: [existingSocket],
     });
-    /*     socket.on("text-data", (data: any) => {
-            console.log(data.text)
-        }) */
+  });
 
-    socket.on("disconnect", () => {
-        activeSockets = activeSockets.filter(
-            existingSocket => existingSocket !== socket.id
-        );
-        socket.broadcast.emit("remove-user", {
-            socketId: socket.id
-        });
+  socket.on('call-user', (data: any) => {
+    socket.to(data.to).emit('call-made', {
+      offer: data.offer,
+      user: data.user,
     });
+  });
 
-})
+  socket.on('make-answer', (data: any) => {
+    socket.to(data.to).emit('answer-made', {
+      socket: socket.id,
+      answer: data.answer,
+    });
+  });
 
-
-process.on("uncaughtException", e => {
-    console.log(e);
-    process.exit(1);
+  socket.on('disconnect', () => {
+    activeSockets = activeSockets.filter(
+      (existingSocket) => existingSocket.id !== socket.id,
+    );
+    socket.broadcast.emit('remove-user', {
+      socketId: socket.id,
+    });
+  });
 });
-process.on("unhandledRejection", e => {
-    console.log(e);
-    process.exit(1);
+
+process.on('uncaughtException', (e) => {
+  console.log(e);
+  process.exit(1);
+});
+process.on('unhandledRejection', (e) => {
+  console.log(e);
+  process.exit(1);
 });
